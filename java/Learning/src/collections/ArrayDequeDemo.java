78d29cb07fb383bffb0b67f4641d3befcf5811c6
Ravinder Bhardwaj <rkbhardwaj88@gmail.com>
2020-08-29 15:13:17 +0530

    Collections

--- java/Learning/src/collections/ArrayDequeDemo.java
+++ java/Learning/src/collections/ArrayDequeDemo.java
@@ -1,1 +1,21 @@
 package collections;
+
+import java.util.ArrayDeque;
+
+public class ArrayDequeDemo {
+	public static void main(String args[]) {
+		// Create an array deque.
+		ArrayDeque<String> adq = new ArrayDeque<String>();
+		// Use an ArrayDeque like a stack.
+		adq.push("A");
+		adq.push("B");
+		adq.push("D");
+		adq.push("E");
+		adq.push("F");
+
+		System.out.print("Popping the stack: ");
+		while (adq.peek() != null)
+			System.out.print(adq.pop() + " ");
+		System.out.println();
+	}
+}
